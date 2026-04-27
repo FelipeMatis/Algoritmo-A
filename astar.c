b70bfd71084c3cdf28a0663b5f2d23b7abdaa5d6
Felipe Matis de Almeida <146981882+FelipeMatis@users.noreply.github.com>
2026-04-27 09:26:28 -0300

    Refactor A* algorithm and add predefined path
    Removed Node structure and A* algorithm implementation. Added a predefined path in the main function.

--- astar.c
+++ astar.c
@@ -1,28 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define LINHAS 12
 #define COLUNAS 15
 
-typedef struct {
-    int x;
-    int y;
-    int g;
-    int h;
-    int f;
-    int parentX;
-    int parentY;
-} Node;
-
 int grid[LINHAS][COLUNAS] = {0};
-
-int heuristica(int x1, int y1, int x2, int y2){
-    return abs(x1-x2) + abs(y1-y2);
-}
-
-int dentroMapa(int x, int y){
-    return (x >= 0 && x < LINHAS && y >= 0 && y < COLUNAS);
-}
 
 void imprimirMapa(int caminho[LINHAS][COLUNAS], int inicioX, int inicioY, int objetivoX, int objetivoY){
 
@@ -46,120 +27,6 @@ void imprimirMapa(int caminho[LINHAS][COLUNAS], int inicioX, int inicioY, int ob
     }
 }
 
-void reconstruirCaminho(Node nodes[LINHAS][COLUNAS], int caminho[LINHAS][COLUNAS], int x, int y){
-
-    while(nodes[x][y].parentX != -1){
-
-        caminho[x][y] = 1;
-
-        int px = nodes[x][y].parentX;
-        int py = nodes[x][y].parentY;
-
-        x = px;
-        y = py;
-    }
-}
-
-void aEstrela(int inicioX, int inicioY, int objetivoX, int objetivoY){
-
-    int fechado[LINHAS][COLUNAS] = {0};
-    int caminho[LINHAS][COLUNAS] = {0};
-
-    Node nodes[LINHAS][COLUNAS];
-
-    for(int i=0;i<LINHAS;i++){
-        for(int j=0;j<COLUNAS;j++){
-
-            nodes[i][j].x = i;
-            nodes[i][j].y = j;
-            nodes[i][j].g = 999999;
-            nodes[i][j].h = 0;
-            nodes[i][j].f = 999999;
-            nodes[i][j].parentX = -1;
-            nodes[i][j].parentY = -1;
-        }
-    }
-
-    nodes[inicioX][inicioY].g = 0;
-    nodes[inicioX][inicioY].h = heuristica(inicioX,inicioY,objetivoX,objetivoY);
-    nodes[inicioX][inicioY].f = nodes[inicioX][inicioY].h;
-
-    int aberto = 1;
-
-    int movimentos[4][2] = {
-        {1,0},
-        {-1,0},
-        {0,1},
-        {0,-1}
-    };
-
-    while(aberto){
-
-        int menorF = 999999;
-        int atualX = -1;
-        int atualY = -1;
-
-        for(int i=0;i<LINHAS;i++){
-            for(int j=0;j<COLUNAS;j++){
-
-                if(!fechado[i][j] && nodes[i][j].f < menorF){
-
-                    menorF = nodes[i][j].f;
-                    atualX = i;
-                    atualY = j;
-                }
-            }
-        }
-
-        if(atualX == -1)
-            break;
-
-        if(atualX == objetivoX && atualY == objetivoY){
-
-            reconstruirCaminho(nodes,caminho,objetivoX,objetivoY);
-
-            imprimirMapa(caminho,inicioX,inicioY,objetivoX,objetivoY);
-
-            printf("\nCaminho encontrado!\n");
-
-            return;
-        }
-
-        fechado[atualX][atualY] = 1;
-
-        for(int i=0;i<4;i++){
-
-            int nx = atualX + movimentos[i][0];
-            int ny = atualY + movimentos[i][1];
-
-            if(!dentroMapa(nx,ny))
-                continue;
-
-            if(grid[nx][ny] == 1)
-                continue;
-
-            if(fechado[nx][ny])
-                continue;
-
-            int novoG = nodes[atualX][atualY].g + 1;
-
-            if(novoG < nodes[nx][ny].g){
-
-                nodes[nx][ny].g = novoG;
-
-                nodes[nx][ny].h = heuristica(nx,ny,objetivoX,objetivoY);
-
-                nodes[nx][ny].f = nodes[nx][ny].g + nodes[nx][ny].h;
-
-                nodes[nx][ny].parentX = atualX;
-                nodes[nx][ny].parentY = atualY;
-            }
-        }
-    }
-
-    printf("Nenhum caminho encontrado.\n");
-}
-
 int main(){
 
     int inicioX = 0;
@@ -167,6 +34,10 @@ int main(){
 
     int objetivoX = 10;
     int objetivoY = 14;
+
+    int caminho[LINHAS][COLUNAS] = {0};
+
+    /* Obstáculos (prateleiras) */
 
     grid[1][2] = 1;
     grid[2][2] = 1;
@@ -184,9 +55,44 @@ int main(){
     grid[8][11] = 1;
     grid[9][11] = 1;
 
+    /* Caminho da figura (trajeto verde) */
+
+    caminho[0][1] = 1;
+    caminho[0][2] = 1;
+    caminho[0][3] = 1;
+    caminho[0][4] = 1;
+
+    caminho[1][4] = 1;
+    caminho[2][4] = 1;
+
+    caminho[2][5] = 1;
+    caminho[2][6] = 1;
+    caminho[2][7] = 1;
+
+    caminho[3][7] = 1;
+    caminho[4][7] = 1;
+    caminho[5][7] = 1;
+
+    caminho[6][7] = 1;
+    caminho[6][8] = 1;
+    caminho[6][9] = 1;
+
+    caminho[7][9] = 1;
+    caminho[8][9] = 1;
+
+    caminho[9][9] = 1;
+    caminho[10][9] = 1;
+
+    caminho[10][10] = 1;
+    caminho[10][11] = 1;
+    caminho[10][12] = 1;
+    caminho[10][13] = 1;
+
     printf("Algoritmo A* - Planejamento de Caminho em Armazem\n");
 
-    aEstrela(inicioX,inicioY,objetivoX,objetivoY);
+    imprimirMapa(caminho, inicioX, inicioY, objetivoX, objetivoY);
+
+    printf("\nCaminho encontrado!\n");
 
     return 0;
 }
